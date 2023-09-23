73f4832c728e8f780351db376b268db58f6b8f03
staimov <staimov@mail.ru>
2023-09-23 23:12:02 +0500

    PagingAndSortingRepository added

--- src/main/java/com/staimov/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/staimov/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,10 +2,13 @@ package com.staimov.springboot.cruddemo.dao;
 
 import com.staimov.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+public interface EmployeeRepository extends
+        JpaRepository<Employee, Integer>,
+        PagingAndSortingRepository<Employee, Integer> {
     // that's it... no need to write any code LOL!
 
     // add a method to sort by last name
