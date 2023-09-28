71bdb6b8b40090e9cb2d30b0f23b040754ce2238
staimov <staimov@mail.ru>
2023-09-28 18:07:30 +0500

    rest controllers updated

--- src/main/java/com/staimov/employee_directory/controller/DepartmentRESTController.java
+++ src/main/java/com/staimov/employee_directory/controller/DepartmentRESTController.java
@@ -1,67 +1,137 @@
 package com.staimov.employee_directory.controller;
 
 import com.staimov.employee_directory.entity.Department;
+import com.staimov.employee_directory.entity.Employee;
 import com.staimov.employee_directory.service.DepartmentService;
+import com.staimov.employee_directory.service.EmployeeService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/api/departments")
 public class DepartmentRESTController {
-    private DepartmentService departmentService;
+    private final EmployeeService employeeService;
+    private final DepartmentService departmentService;
+    private final DepartmentModelAssembler departmentModelAssembler;
+    private final EmployeeModelAssembler employeeModelAssembler;
 
-    public DepartmentRESTController(DepartmentService departmentService) {
+    public DepartmentRESTController(EmployeeService employeeService,
+                                    DepartmentService departmentService,
+                                    EmployeeModelAssembler employeeModelAssembler,
+                                    DepartmentModelAssembler departmentModelAssembler) {
+
+        this.employeeService = employeeService;
         this.departmentService = departmentService;
+        this.departmentModelAssembler = departmentModelAssembler;
+        this.employeeModelAssembler = employeeModelAssembler;
     }
 
     @GetMapping("")
-    public List<Department> findAll(HttpServletRequest request) {
-        return departmentService.findAll();
+    public CollectionModel<EntityModel<Department>> getAllDepartments(HttpServletRequest request) {
+
+        List<EntityModel<Department>> departmentModels = departmentService.findAll().stream()
+                .map(departmentModelAssembler::toModel)
+                .collect(Collectors.toList());
+
+        Link selfLink = linkTo(methodOn(DepartmentRESTController.class).getAllDepartments(null))
+                .withSelfRel();
+
+        return CollectionModel.of(departmentModels, selfLink);
     }
 
-    @GetMapping("/{id}")
-    public Department findById(HttpServletRequest request, @PathVariable int id) {
-        Department department = departmentService.findById(id);
+    @GetMapping("/{departmentId}")
+    public EntityModel<Department> getDepartmentById(HttpServletRequest request,
+                                                     @PathVariable int departmentId) {
+
+        Department department = departmentService.findById(departmentId);
         if (department == null) {
-            throw new RuntimeException("Department id not found: " + id);
+            throw new ResourceNotFoundException("Could not find department " + departmentId);
         }
 
-        return department;
+        return departmentModelAssembler.toModel(department);
+    }
+
+    @GetMapping("/{departmentId}/employees")
+    public CollectionModel<EntityModel<Employee>> getEmployeesByDepartmentId(HttpServletRequest request,
+                                                             @PathVariable int departmentId) {
+
+        Department department = departmentService.findById(departmentId);
+        if (department == null) {
+            throw new ResourceNotFoundException("Could not find department " + departmentId);
+        }
+
+        Set<Employee> employees = department.getEmployees();
+        List<EntityModel<Employee>> employeeModels = employees.stream()
+                .map(employeeModelAssembler::toModel)
+                .collect(Collectors.toList());
+
+        Link selfLink = linkTo(methodOn(DepartmentRESTController.class).getEmployeesByDepartmentId(null, departmentId))
+                .withSelfRel();
+
+        return CollectionModel.of(employeeModels, selfLink);
     }
 
     @PostMapping("")
-    public Department addDepartment(HttpServletRequest request, @RequestBody Department department) {
+    public ResponseEntity<?> addDepartment(HttpServletRequest request, @RequestBody Department newDepartment) {
         // also just in case they pass an id in JSON ... set id to 0
         // this is to force a save of new item ... instead of update
+        newDepartment.setId(0);
 
-        department.setId(0);
+        EntityModel<Department> departmentModel = departmentModelAssembler
+                .toModel(departmentService.save(newDepartment));
 
-        Department dbDepartment = departmentService.save(department);
-
-        return dbDepartment;
+        return ResponseEntity
+                .created(departmentModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(departmentModel);
     }
 
-    @PutMapping("")
-    public Department updateDepartment(HttpServletRequest request, @RequestBody Department department) {
-        Department updatedDepartment = departmentService.save(department);
+    @PutMapping("/{departmentId}")
+    public ResponseEntity<?> updateDepartment(HttpServletRequest request,
+                                       @RequestBody Department department,
+                                       @PathVariable int departmentId) {
 
-        return updatedDepartment;
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(HttpServletRequest request, @PathVariable int id) {
-        Department foundDepartment = departmentService.findById(id);
-
-        if (foundDepartment == null) {
-            throw new RuntimeException("Department id not found: " + id);
+        Department departmentToUpdate = departmentService.findById(departmentId);
+        if (departmentToUpdate == null) {
+            throw new ResourceNotFoundException("Could not find department " + departmentId);
         }
 
-        departmentService.deleteById(id);
+        departmentToUpdate.setName(department.getName());
 
-        return "Deleted department with id: " + id;
+        EntityModel<Department> departmentModel = departmentModelAssembler
+                .toModel(departmentService.save(departmentToUpdate));
+
+        return ResponseEntity
+                .created(departmentModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(departmentModel);
+    }
+
+    @DeleteMapping("/{departmentId}")
+    public ResponseEntity<?> deleteDepartment(HttpServletRequest request,
+                                   @PathVariable int departmentId) {
+
+        Department departmentToDelete = departmentService.findById(departmentId);
+
+        if (departmentToDelete == null) {
+            throw new ResourceNotFoundException("Could not find department " + departmentId);
+        }
+
+        departmentService.deleteById(departmentId);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/staimov/employee_directory/controller/EmployeeRESTController.java
+++ src/main/java/com/staimov/employee_directory/controller/EmployeeRESTController.java
@@ -5,83 +5,146 @@ import com.staimov.employee_directory.entity.Employee;
 import com.staimov.employee_directory.service.DepartmentService;
 import com.staimov.employee_directory.service.EmployeeService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/api/employees")
 public class EmployeeRESTController {
 
-    private EmployeeService employeeService;
-    private DepartmentService departmentService;
+    private final EmployeeService employeeService;
+    private final DepartmentService departmentService;
+    private final EmployeeModelAssembler employeeModelAssembler;
 
-    public EmployeeRESTController(EmployeeService employeeService, DepartmentService departmentService) {
+    public EmployeeRESTController(EmployeeService employeeService,
+                                  DepartmentService departmentService,
+                                  EmployeeModelAssembler employeeModelAssembler) {
+
         this.employeeService = employeeService;
         this.departmentService = departmentService;
+        this.employeeModelAssembler = employeeModelAssembler;
     }
 
     @GetMapping("")
-    public List<Employee> findAll(HttpServletRequest request) {
-        return employeeService.findAll();
+    public CollectionModel<EntityModel<Employee>> getAllEmployees(HttpServletRequest request) {
+
+        List<EntityModel<Employee>> employeeModels = employeeService.findAll().stream()
+                .map(employeeModelAssembler::toModel)
+                .collect(Collectors.toList());
+
+        Link selfLink = linkTo(methodOn(EmployeeRESTController.class).getAllEmployees(null))
+                .withSelfRel();
+
+        return CollectionModel.of(employeeModels, selfLink);
     }
 
-    @GetMapping("/{id}")
-    public Employee findById(HttpServletRequest request, @PathVariable int id) {
-        Employee employee = employeeService.findById(id);
+    @GetMapping("/{employeeId}")
+    public EntityModel<Employee> getEmployeeById(HttpServletRequest request,
+                                                    @PathVariable int employeeId) {
+
+        Employee employee = employeeService.findById(employeeId);
         if (employee == null) {
-            throw new RuntimeException("Employee id not found: " + id);
+            throw new ResourceNotFoundException("Could not find employee " + employeeId);
         }
 
-        return employee;
+        return employeeModelAssembler.toModel(employee);
+    }
+
+    @GetMapping("/{employeeId}/department")
+    public EntityModel<Department> getDepartmentByEmployeeId(HttpServletRequest request,
+                                                             @PathVariable int employeeId) {
+
+        Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new ResourceNotFoundException("Could not find employee " + employeeId);
+        }
+
+        Department department = employee.getDepartment();
+        EntityModel<Department> departmentModel = EntityModel.of(department);
+
+        departmentModel.add(linkTo(methodOn(DepartmentRESTController.class).getAllDepartments(null))
+                .withRel("departments"));
+
+        departmentModel.add(linkTo(methodOn(EmployeeRESTController.class).getDepartmentByEmployeeId(null, employeeId))
+                .withSelfRel());
+
+        return departmentModel;
     }
 
     @PostMapping("")
-    public Employee addEmployee(HttpServletRequest request, @RequestBody Employee employee) {
+    public ResponseEntity<?> addEmployee(HttpServletRequest request, @RequestBody Employee newEmployee) {
         // also just in case they pass an id in JSON ... set id to 0
         // this is to force a save of new item ... instead of update
+        newEmployee.setId(0);
 
-        employee.setId(0);
+        int departmentId = newEmployee.getDepartment().getId();
+        Department foundDepartment = departmentService.findById(departmentId);
+        if (foundDepartment == null) {
+            throw new ResourceNotFoundException("Could not find department " + departmentId);
+        }
+
+        newEmployee.setDepartment(foundDepartment);
+
+        EntityModel<Employee> employeeModel = employeeModelAssembler
+                .toModel(employeeService.save(newEmployee));
+
+        return ResponseEntity
+                .created(employeeModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(employeeModel);
+    }
+
+    @PutMapping("/{employeeId}")
+    public ResponseEntity<?> updateEmployee(HttpServletRequest request,
+                                            @RequestBody Employee employee,
+                                            @PathVariable int employeeId) {
+
+        Employee employeeToUpdate = employeeService.findById(employeeId);
+        if (employeeToUpdate == null) {
+            throw new ResourceNotFoundException("Could not find employee " + employeeId);
+        }
 
         int departmentId = employee.getDepartment().getId();
         Department foundDepartment = departmentService.findById(departmentId);
         if (foundDepartment == null) {
-            throw new RuntimeException("Department id not found: " + departmentId);
+            throw new ResourceNotFoundException("Could not find department " + departmentId);
         }
 
-        employee.setDepartment(foundDepartment);
-        Employee dbEmployee = employeeService.save(employee);
+        employeeToUpdate.setDepartment(foundDepartment);
+        employeeToUpdate.setEmail(employee.getEmail());
+        employeeToUpdate.setFirstName(employee.getFirstName());
+        employeeToUpdate.setLastName(employee.getLastName());
 
-        return dbEmployee;
+        EntityModel<Employee> employeeModel = employeeModelAssembler
+                .toModel(employeeService.save(employeeToUpdate));
+
+        return ResponseEntity
+                .created(employeeModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(employeeModel);
     }
 
-    @PutMapping("")
-    public Employee updateEmployee(HttpServletRequest request, @RequestBody Employee employee) {
-        int departmentId = employee.getDepartment().getId();
-        Department foundDepartment = departmentService.findById(departmentId);
-        if (foundDepartment == null) {
-            throw new RuntimeException("Department id not found: " + departmentId);
+    @DeleteMapping("/{employeeId}")
+    public ResponseEntity<?> deleteEmployeeById(HttpServletRequest request,
+                                                @PathVariable int employeeId) {
+
+        Employee employeeToDelete = employeeService.findById(employeeId);
+
+        if (employeeToDelete == null) {
+            throw new ResourceNotFoundException("Could not find employee " + employeeId);
         }
 
-        employee.setDepartment(foundDepartment);
+        employeeService.deleteById(employeeId);
 
-        Employee updatedEmployee = employeeService.save(employee);
-
-        return updatedEmployee;
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(HttpServletRequest request, @PathVariable int id) {
-        Employee foundEmployee = employeeService.findById(id);
-
-        if (foundEmployee == null) {
-            throw new RuntimeException("Employee id not found: " + id);
-        }
-
-        employeeService.deleteById(id);
-
-        return "Deleted employee with id: " + id;
+        return ResponseEntity.noContent().build();
     }
 }
