8e186ddc9381779c68868d34c566b3968208f5dd
staimov <staimov@mail.ru>
2023-09-28 18:07:08 +0500

    modelassemnblers added

---
+++ src/main/java/com/staimov/employee_directory/controller/EmployeeModelAssembler.java
@@ -0,0 +1,25 @@
+package com.staimov.employee_directory.controller;
+
+import com.staimov.employee_directory.entity.Employee;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+    @Override
+    public EntityModel<Employee> toModel(Employee employee) {
+        int employeeId = employee.getId();
+        return EntityModel.of(employee,
+                linkTo(methodOn(EmployeeRESTController.class).getEmployeeById(null, employeeId))
+                        .withSelfRel(),
+                linkTo(methodOn(EmployeeRESTController.class).getAllEmployees(null))
+                        .withRel("employees"),
+                linkTo(methodOn(EmployeeRESTController.class).getDepartmentByEmployeeId(null, employeeId))
+                        .withRel("department"));
+    }
+}
